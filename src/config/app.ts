d3923725804ff5a5e0d6fa5777b29f2acc27de58
pietrodev07 <pietro.dev.07@gmail.com>
2024-09-05 17:54:10 +0200

    chore(config-app): add app config object

---
+++ src/config/app.ts
@@ -0,0 +1,29 @@
+import { env } from "./env";
+
+const smtpConfig = {
+  host: env.EMAIL_HOST,
+  service: env.EMAIL_SERVICE,
+  port: env.EMAIL_PORT,
+  secure: env.EMAIL_SECURE,
+  auth: { user: env.EMAIL_USER, pass: env.EMAIL_PASSWORD },
+};
+
+const corsConfig = {
+  origin: [env.FRONTEND_URL],
+  allowHeaders: ["Access-Control-Allow-Origin", "Content-Type"],
+  allowMethods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
+
+export const appConfig = {
+  corsConfig,
+  smtpConfig,
+  databaseUrl: env.DATABASE_URL,
+  serverPort: env.SERVER_PORT,
+  saltRounds: 10,
+  frontendURL: env.FRONTEND_URL,
+  jwtSecretKey: env.JWT_SECRET_KEY,
+  accessToken: 3600 * 60,
+  verifyToken: 3600 * 60,
+  resetToken: 3600 * 60,
+};
