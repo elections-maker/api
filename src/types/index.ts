eb5b7abdac8a3236cc1ac6cd48aa3d5c38e4ec61
pietrodev07 <pietro.dev.07@gmail.com>
2024-09-05 18:14:04 +0200

    feat(utils-router): add router util function

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,15 @@
+import { ZodType } from "zod";
+import { MiddlewareHandler } from "hono";
+import { H, ValidationTargets } from "hono/types";
+
+export type RouteData = {
+  path: string;
+  handler: [H];
+  validators?: { type: keyof ValidationTargets; schema: ZodType }[];
+  method: "GET" | "POST" | "PUT" | "DELETE";
+  middlewares?: MiddlewareHandler[];
+};
+
 export type EmailOptions = {
   to: string;
   subject: string;
