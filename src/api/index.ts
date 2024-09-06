ce7fd6a9b82d6a6bcce9cd82a30c8674f258cc17
pietrodev07 <pietro.dev.07@gmail.com>
2024-09-06 18:44:15 +0200

    feat(api-cms-lists): add lists router

--- src/api/index.ts
+++ src/api/index.ts
@@ -7,6 +7,7 @@ import { authRouter } from "./base/auth/auth.routes";
 import { organizationsRouter } from "./base/organizations/organizations.routes";
 
 import { usersRouter } from "./cms/users/users.routes";
+import { listsRouter } from "./cms/lists/lists.routes";
 
 export const appRouter = new Hono().basePath("/v1");
 
@@ -17,3 +18,4 @@ appRouter.route("auth", authRouter);
 appRouter.route("admin/organizations", organizationsRouter);
 
 appRouter.route("admin/organizations/:organizationId/users", usersRouter);
+appRouter.route("admin/organizations/:organizationId/lists", listsRouter);
