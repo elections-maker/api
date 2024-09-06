20c06025ea156d08ba82fb096c325bbfe16d4dde
pietrodev07 <pietro.dev.07@gmail.com>
2024-09-06 18:03:55 +0200

    feat(api-base-organizations): add organizations router

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,15 @@
 import { Hono } from "hono";
 
+import { authMiddleware } from "@/middlewares/auth.middleware";
+import { organizationMiddleware } from "@/middlewares/organization.middleware";
+
 import { authRouter } from "./base/auth/auth.routes";
+import { organizationsRouter } from "./base/organizations/organizations.routes";
 
 export const appRouter = new Hono().basePath("/v1");
 
+appRouter.use("admin/organizations/*", authMiddleware);
+appRouter.use("admin/organizations/:organizationId/*", organizationMiddleware);
+
 appRouter.route("auth", authRouter);
+appRouter.route("admin/organizations", organizationsRouter);
