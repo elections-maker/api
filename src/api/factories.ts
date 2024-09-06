19b2fca9d56a4d96bf31731153c56a616da20aa1
pietrodev07 <pietro.dev.07@gmail.com>
2024-09-06 17:56:07 +0200

    fix(factories): adjust the types of the organization factory

--- src/api/factories.ts
+++ src/api/factories.ts
@@ -1,16 +1,17 @@
 import { createFactory } from "hono/factory";
 import { AccessDecodedToken, Env } from "@/types";
-import { PrismaClient as MainClient } from "@/database/generated/main";
+import { Organization } from "@/database/generated/main";
 import { PrismaClient as OrgClient } from "@/database/generated/organization";
 
 type AuthEnv = Env<{ userData: AccessDecodedToken }>;
 
-type OrgEnv = Env<{
-  orgData: {
-    database: OrgClient;
-    data: MainClient["organization"];
-  };
-}>;
+type OrgEnv = AuthEnv &
+  Env<{
+    orgData: {
+      database: OrgClient;
+      data: Organization;
+    };
+  }>;
 
 export const baseFactory = createFactory();
 
