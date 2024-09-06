bf8c145588cbe2c241f773df71d395974a6ed394
pietrodev07 <pietro.dev.07@gmail.com>
2024-09-06 18:07:55 +0200

    feat(api-base-organizations): add get-all-organizations service

--- src/api/base/organizations/organizations.routes.ts
+++ src/api/base/organizations/organizations.routes.ts
@@ -2,8 +2,14 @@ import { generateRouter } from "@/utils/router";
 import * as schemas from "./organizations.schemas";
 
 import { createOrganizationService } from "./services/create.service";
+import { getAllOrganizationsService } from "./services/get-all.service";
 
 export const organizationsRouter = generateRouter([
+  {
+    path: "/",
+    method: "GET",
+    handler: getAllOrganizationsService,
+  },
   {
     path: "/",
     method: "POST",
---
+++ src/api/base/organizations/services/get-all.service.ts
@@ -0,0 +1,17 @@
+import { db } from "@/database";
+import { authFactory } from "@/api/factories";
+
+export const getAllOrganizationsService = authFactory.createHandlers(async (c) => {
+  const { userId } = c.get("userData");
+
+  const fetchedOrgs = await db.organization.findMany({
+    where: { userId },
+    orderBy: { updatedAt: "desc" },
+  });
+
+  return c.json({
+    success: true,
+    message: "Organizations fetched successfully",
+    data: { orgs: fetchedOrgs },
+  });
+});
