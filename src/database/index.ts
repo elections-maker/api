674d1641bd1466e1043b6c9c787f71e698ab4e35
pietrodev07 <pietro.dev.07@gmail.com>
2024-09-06 08:58:14 +0200

    feat(database): add create org client

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,15 @@
+import { appConfig } from "@/config/app";
 import { PrismaClient as MainClient } from "./generated/main";
+import { PrismaClient as OrgClient } from "./generated/organization";
 
 export const db = new MainClient();
+
+export const createOrgClient = (dbName: string) => {
+  return new OrgClient({
+    datasources: {
+      db: {
+        url: `${appConfig.baseOrgDatabaseUrl}/${dbName}`,
+      },
+    },
+  });
+};
