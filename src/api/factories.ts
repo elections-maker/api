45b16ee044f22fe7a9802b7c5d954af05e9e2c45
pietrodev07 <pietro.dev.07@gmail.com>
2024-09-06 09:15:06 +0200

    feat(factories): create organization factory

--- src/api/factories.ts
+++ src/api/factories.ts
@@ -1,8 +1,18 @@
 import { createFactory } from "hono/factory";
 import { AccessDecodedToken, Env } from "@/types";
+import { PrismaClient as MainClient } from "@/database/generated/main";
+import { PrismaClient as OrgClient } from "@/database/generated/organization";
 
 type AuthEnv = Env<{ userData: AccessDecodedToken }>;
+
+type OrgEnv = Env<{
+  orgData: {
+    database: OrgClient;
+    data: MainClient["organization"];
+  };
+}>;
 
 export const baseFactory = createFactory();
 
 export const authFactory = createFactory<AuthEnv>();
+export const orgFactory = createFactory<OrgEnv>();
