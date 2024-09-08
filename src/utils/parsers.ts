6f4a8a8b62a01de3cbcc32f2d88536b097320921
pietrodev07 <pietro.dev.07@gmail.com>
2024-09-08 16:13:01 +0200

    feat(utils-parsers): add csv parser util function

--- src/utils/parsers.ts
+++ src/utils/parsers.ts
@@ -1,3 +1,15 @@
+import { parse as csvParse } from "csv-parse/sync";
+
 export const jsonParser = <T>(data: string) => {
   return JSON.parse(data) as T[];
+};
+
+export const csvParser = <T>(data: string, delimiter?: string) => {
+  const records = csvParse(data, {
+    columns: true,
+    skip_empty_lines: true,
+    delimiter,
+  });
+
+  return records as T[];
 };
