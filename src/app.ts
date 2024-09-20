7af456731bc23a3bbd62714f5abade36d2e94e9c
pietrodev07 <pietro.dev.07@gmail.com>
2024-09-20 14:46:24 +0200

    feat(api): add error handler

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,20 @@ import { cors } from "hono/cors";
 import { appRouter } from "@/api";
 import { Context, Hono } from "hono";
 import { appConfig } from "@/config/app";
+import { HTTPResponseError } from "hono/types";
 
 export const baseHandler = (c: Context) => {
   return c.json({ success: true, message: "Hello from elections maker api!" }, 200);
 };
 
+export const errorHandler = (_: Error | HTTPResponseError, c: Context) => {
+  return c.json({ success: false, message: "Internal Server Error!" }, 500);
+};
+
 export const bootstrapApplication = () => {
-  return new Hono().use(cors(appConfig.corsConfig)).all("/", baseHandler).route("/api", appRouter);
+  return new Hono()
+    .use(cors(appConfig.corsConfig))
+    .all("/", baseHandler)
+    .onError(errorHandler)
+    .route("/api", appRouter);
 };
