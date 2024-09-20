e95c930ea415ac8e5ce5376948cbc1c8e5be47b2
pietrodev07 <pietro.dev.07@gmail.com>
2024-09-20 17:06:44 +0200

    feat(rabbitmq): create upload-users rabbit consumer

--- src/rabbitmq/index.ts
+++ src/rabbitmq/index.ts
@@ -6,3 +6,13 @@ export const rabbit = new RabbitMQ(appConfig.rabbitmqUrl)
   .on("connection", () => console.log("connection successfully (re)established"));
 
 export const publisher = rabbit.createPublisher({ confirm: true });
+
+rabbit.generateConsumers([
+  {
+    props: { queue: "upload-users" },
+    handler: async (msg) => {
+      console.log("received message (upload-users)", msg);
+      // TODO: upload users
+    },
+  },
+]);
