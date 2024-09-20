bb88156fec1e2b5abbe627fcbbba5ef5e993641e
pietrodev07 <pietro.dev.07@gmail.com>
2024-09-20 14:46:03 +0200

    feat(api): add base handler

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,12 @@
-import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { appRouter } from "@/api";
+import { Context, Hono } from "hono";
 import { appConfig } from "@/config/app";
 
+export const baseHandler = (c: Context) => {
+  return c.json({ success: true, message: "Hello from elections maker api!" }, 200);
+};
+
 export const bootstrapApplication = () => {
-  const app = new Hono();
-
-  app.use(cors(appConfig.corsConfig));
-
-  app.route("/api", appRouter);
-
-  return app;
+  return new Hono().use(cors(appConfig.corsConfig)).all("/", baseHandler).route("/api", appRouter);
 };
